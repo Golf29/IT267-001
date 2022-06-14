759ba7422dbfeba66372724f8cf81ddb94b1b47c
Golf29 <640108020006@dpu.ac.th>
2022-06-14 13:50:20 +0700

    person

--- person.py
+++ person.py
@@ -1,5 +1,10 @@
+from time import perf_counter
+
+
 class Person:
+    country = 'Thailand' #class variable
     def __init__(self,name,gender,profession,hours) -> None:
+        #instance
         self.name = name
         self.gender = gender
         self.profession = profession
@@ -15,11 +20,26 @@ jessa = Person('Jessa','male','Software engineer','10')
 jessa.show()
 jessa.work()
 jessa.study()
-
+print('----------------------------')
 Jon = Person('Jon','male','Doctor','15')
 Jon.show()
 Jon.work()
 Jon.study()
-
+print('----------------------------')
 lalisa = Person('lalisa','female','Korean Singer','13')
 lalisa.work()
+Person.country
+print(f'Class Variable: {Person.country}')
+print(f'Instance Variable: {lalisa.country}')
+#assign value
+lalisa.country = 'Korea'
+print('----------------------------')
+print(f'Class Variable: {Person.country}')
+print(f'Instance Variable: {lalisa.country}')
+print(f'Instance Variable: {Jon.country}')
+
+Person.country = 'England'
+print('----------------------------')
+print(f'Class Variable: {Person.country}')
+print(f'Instance Variable: {lalisa.country}')
+print(f'Instance Variable: {Jon.country}')
