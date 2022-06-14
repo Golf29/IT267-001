d6bbaf8f75034ad60323947f19eec23b05711f80
Golf29 <640108020006@dpu.ac.th>
2022-06-14 13:50:05 +0700

    animal

---
+++ animal.py
@@ -0,0 +1,32 @@
+class Animal:
+    animal = 'CAT'
+
+    def new_animal(self,name:str,breed:str,colour:str,age:int):
+        self.name = name
+        self.breed = breed
+        self.colour = colour
+        self.age = age
+
+
+    def print_detail(self):
+        print(f'-----------------------------------')
+        print(f'name:{self.name}')
+        print(f'animal:{self.animal}')
+        print(f'breed:{self.breed}')
+        print(f'colour:{self.colour}')
+        print(f'age:{self.age}')
+
+if __name__ == '__main__':
+    Animal.animal = 'FISH'
+    ula = Animal()
+    ula.animal = 'DOG'
+    ula.new_animal('ula','Scottish','white',1)
+    ula.print_detail()
+
+    drac = Animal()
+    drac.new_animal('Drac','Scottich','white',1)
+    drac.print_detail()
+    drac.breed = 'Carfish'
+    drac.print_detail()
+    Animal.print_detail(ula)
+    Animal.print_detail(drac)
