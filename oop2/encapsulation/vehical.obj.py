0c73fc8c6a1572dee221d5efff0cfb9c4b455811
Golf29 <640108020006@dpu.ac.th>
2022-06-27 14:18:41 +0700

    new

---
+++ oop2/encapsulation/vehical.obj.py
@@ -0,0 +1,10 @@
+from bus import Bus
+from motocycle import Motocycle
+bus=Bus('Bus',4,120,'v1234')
+bus.set_color('Blue')
+bus.set_capacity(34)
+bus.bus_detail()
+
+bike = Motocycle('Motocycle',2,100,'v5678')
+bike.set_cc(1200)
+bike.bike_detail()
