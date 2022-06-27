72e4c25590532a34487653ff14b5d7a426f55906
Golf29 <640108020006@dpu.ac.th>
2022-06-27 15:19:19 +0700

    new

---
+++ oop2/encapsulation/project/project.py
@@ -0,0 +1,16 @@
+class Project:
+    def __init__(self,name,time,location) -> None:
+        self.name = name
+        self.time = time
+        self._location = location
+        self.__budget = 50
+
+    def show(self):
+        print('===================================================================================')
+        print(f'name: {self.name}')
+        print(f'time: {self.time}months')
+        print(f'location: {self._location}')
+    def get_budget(self):
+        return self.__budget
+    def update_budget(self,budget):
+        self.__budget = budget
---
+++ oop2/encapsulation/project/project_called.py
@@ -0,0 +1,9 @@
+from condo import Condo
+ideo = Condo('ideo5',18,'Ladphrao',80,'Condo 20 Floors')
+ideo.show()
+
+from project import Project
+home = Project('Ladawan',15,'Bang Yai')
+home.show()
+home.update_budget(30)
+print (f'home: {home.get_budget()}MB')  
